125c60cd22fa5d08727443f00a7477f734ceff49
Phillip Rosen <phill.rosen@gmail.com>
2012-04-23 01:59:24 -0400

    tweeters have publications, can be editted

--- routes/publications.js
+++ routes/publications.js
@@ -10,12 +10,20 @@
 
 exports.publicationsList = function (req, res) {
     var start = parseInt(req.query.start || 0)
+    var filter = req.query.filter || {}
+    if (typeof filter == 'string')
+        filter = JSON.parse(filter)
+    if (filter.whist_list_status) {
+        filter.whist_list_status = filter.whist_list_status.toString()
+    }
+
+
     var perpage = 20
     var previous = 0
     previous = (start - perpage) > 0 ? start - perpage : 0
 
     models.Publication.count(function (countError, count) {
-        models.Publication.find().skip(start).limit(perpage).sort('createdAt', 'descending').execFind(function (err, results) {
+        models.Publication.find(filter).skip(start).limit(perpage).sort('createdAt', 'descending').execFind(function (err, results) {
             if (err) {
                 res.send(err)
             } else {
@@ -24,6 +32,7 @@ exports.publicationsList = function (req, res) {
                     total:count,
                     start:start + perpage,
                     previous:previous,
+                    filter:filter
                     //  MediaAmpModels:MediaAmpModels
                 })
             }
@@ -38,13 +47,13 @@ exports.publicationShow = function (req, res) {
         if (err) {
             res.send(err)
         } else {
-            models.Tweeter.find({ma_publications:_id},function(err,tweeters){
+            models.Tweeter.find({ma_publications:_id}, function (err, tweeters) {
 
-            res.render('publications/publications_show', {
-                publication:publication,
-                tweeters:tweeters||[]
+                res.render('publications/publications_show', {
+                    publication:publication,
+                    tweeters:tweeters || []
 
-            })
+                })
             })
         }
     })
@@ -74,43 +83,43 @@ exports.publicationNew = function (req, res) {
 
 exports.publicationSave = function (req, res) {
     var publication
-    if(req.body.host_name){
-        req.body.host_name=req.body.host_name.toLocaleLowerCase().replace(/(http|https)\:\/\//gi,'')
+    if (req.body.host_name) {
+        req.body.host_name = req.body.host_name.toLocaleLowerCase().replace(/(http|https)\:\/\//gi, '')
     }
 
-    if (req.body._id == '' || (typeof req.body._id == undefined)){
+    if (req.body._id == '' || (typeof req.body._id == undefined)) {
         //its a new publication
         delete req.body._id
         publication = new models.Publication(req.body)
         publication.save(function (err, result) {
-                if (err) {
-                    res.send(err)
-                    console.log(err)
-                } else {
-                    var _id = result._id
-                    maHelper.updatePublicationProfile(result, function (err, result) {
-                        if (err) {
-                            res.send(err)
-                        } else {
-                            res.redirect('/publications/show/' + _id)
-                        }
-                    })
+            if (err) {
+                res.send(err)
+                console.log(err)
+            } else {
+                var _id = result._id
+                maHelper.updatePublicationProfile(result, function (err, result) {
+                    if (err) {
+                        res.send(err)
+                    } else {
+                        res.redirect('/publications/show/' + _id)
+                    }
+                })
 
-                }
-            })
+            }
+        })
     }
 
-    else{
+    else {
         //editting existing
-        models.Publication.findById(req.body._id, function(error, publication){
-            for(var field in req.body){
-                if(field!='_id'){
-                    publication._doc[field]=req.body[field]
+        models.Publication.findById(req.body._id, function (error, publication) {
+            for (var field in req.body) {
+                if (field != '_id') {
+                    publication._doc[field] = req.body[field]
                     publication.markModified(field)
                 }
             }
-            publication.save(function(err,result){
-                if(err){
+            publication.save(function (err, result) {
+                if (err) {
                     res.render('publications/publications_new', {
                         page_name:'Create',
                         publication:{},
@@ -118,7 +127,7 @@ exports.publicationSave = function (req, res) {
                             'Error saving':error
                         }
                     })
-                }else{
+                } else {
                     res.redirect('/publications/show/' + publication._id)
                 }
             })
